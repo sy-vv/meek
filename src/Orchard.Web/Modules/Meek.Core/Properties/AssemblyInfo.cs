4baafdb9de197f62fabe57f911fecd6231505204
Michael Dela Cuesta <michadel@efi.com>
2014-03-07 23:14:05 +0800

    added meek modules

---
+++ src/Orchard.Web/Modules/Meek.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Meek.Core")]
+[assembly: AssemblyDescription("")]
+
---
+++ src/Orchard.Web/Modules/Meek.WebStore/Properties/Meek.AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Orchard")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
+
